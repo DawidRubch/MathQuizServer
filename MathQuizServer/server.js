5f79c4d04045e74e490dbc6380f8c2322101bd85
DawidRubch <50677638+DawidRubch@users.noreply.github.com>
2020-04-12 22:25:03 +0200

    Added basic login

--- MathQuizServer/server.js
+++ MathQuizServer/server.js
@@ -1,13 +1,34 @@
-const express = require('express');
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
 
-const app = express();
+app.use(express.json())
 
+const users = []
 
+app.get('/users', (req, res) => {
+    res.json(users)
+})
 
-//Routes
+app.post('/users', async (req, res) => {
+    try {
 
-app.get('/', (req, res) => { res.send('HomePage') });
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        const user = { name: req.body.name, password: hashedPassword }
 
+        console.log(hashedPassword)
+        users.push(user)
+        res.status(201).send()
+    } catch (err) {
+        res.status(500).send()
+        console.log(err)
+    }
+})
+
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.name === req.body.name)
+
+})
 
 //Server listen
-app.listen(3000);
+app.listen(3000)
